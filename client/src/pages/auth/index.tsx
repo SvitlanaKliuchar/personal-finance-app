6a6946e1a8aa3e1c9cf68ef7f7f9598534572b20
SvitlanaKliuchar <svitlanakliuchar@gmail.com>
2024-05-23 14:57:56 -0300

    Create markup for auth page with SignIn SignUp buttons

--- client/src/pages/auth/index.tsx
+++ client/src/pages/auth/index.tsx
@@ -1,12 +1,13 @@
-import {SignedIn, SignedOut, SignInButton, SignUpButtonButton, UserButton} from '@clerk/clerk-react'
+import {SignedIn, SignedOut, SignInButton, SignUpButton, UserButton} from '@clerk/clerk-react'
 
 export const Auth = () => {
     return <div className="sign-in-container">
         <SignedOut>
-
+            <SignInButton mode='modal' />
+            <SignUpButton mode='modal'/>
         </SignedOut>
         <SignedIn>
-            
+            <UserButton />
         </SignedIn>
     </div>
 }
