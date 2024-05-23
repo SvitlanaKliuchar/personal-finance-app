268d8757d3ba876508387e05ffb8a5c5476f5e7a
SvitlanaKliuchar <svitlanakliuchar@gmail.com>
2024-05-23 15:05:24 -0300

    Create two new tsx files for dashboard components

--- client/src/pages/dashboard/index.tsx
+++ client/src/pages/dashboard/index.tsx
@@ -3,8 +3,6 @@ import {useUser} from '@clerk/clerk-react'
 export const Dashboard = () => {
     const {user} = useUser();
 
-
-
     return (<div className="dashboard-container">
         <h1>Welcome {user?.firstName}! Here are your finance records </h1>
     </div>)
