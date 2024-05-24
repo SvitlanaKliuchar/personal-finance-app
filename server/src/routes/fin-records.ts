68eb161170d6b60848751a426844513e08666f26
SvitlanaKliuchar <svitlanakliuchar@gmail.com>
2024-05-24 12:39:25 -0300

    Define a route that handles get request

--- server/src/routes/fin-records.ts
+++ server/src/routes/fin-records.ts
@@ -3,4 +3,19 @@ import FinancialRecordModel from "@src/schema/fin-record"
 
 const router = express.Router()
 
+router.get('/getAllByUserId:userId', async (req: Request, res: Response) => {
+    try {
+        const userId = req.params.userId
+        const records = await FinancialRecordModel.find({userId: userId })
+        if (records.length === 0) {
+            return res.status(404).send("No records found for this user")
+        }
+        res.status(200).send(records)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
 export default router
+
+//define a route
