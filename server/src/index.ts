ca82a449e6bfb58f830c75f5549e21f9e58f4e23
SvitlanaKliuchar <svitlanakliuchar@gmail.com>
2024-05-24 11:27:20 -0300

    Start the server and set up the connection to MongoDB

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,3 +8,17 @@ const app: Express = express()
 const port = process.env.PORT || 3001;
 
 app.use(express.json())
+
+const mongoURI: string = process.env.MongoDBConnectionString ?? '';
+if (!mongoURI) {
+  throw new Error('MongoDBConnectionString environment variable is not set.');
+}
+
+mongoose
+    .connect(mongoURI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('Failed to connect to MongoDB:', err))
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
