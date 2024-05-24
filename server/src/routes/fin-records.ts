bd6b91526aed31018ed374fdc2c51863eaef20e8
SvitlanaKliuchar <svitlanakliuchar@gmail.com>
2024-05-24 12:45:21 -0300

    Define a route that handles post request

--- server/src/routes/fin-records.ts
+++ server/src/routes/fin-records.ts
@@ -16,6 +16,18 @@ router.get('/getAllByUserId:userId', async (req: Request, res: Response) => {
     }
 })
 
+router.post('/', async (req: Request, res: Response) => {
+    try {
+        const newRecordBody = req.body 
+        const newRecord = new FinancialRecordModel(newRecordBody)
+        const savedRecord = await newRecord.save()
+
+        res.status(200).send(savedRecord)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
 export default router
 
 //define a route
